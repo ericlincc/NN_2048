ba70cf919e6ee5ae599aeee390602a3ca77c15ee
ericlincc <lincheukyin_eric@yahoo.com.hk>
2017-08-27 04:58:55 +0800

    Version 1 completed. First trial run. Updated Param

--- Run_Script.py
+++ Run_Script.py
@@ -1,10 +1,16 @@
 import Evolutions as evolution
 import pickle
 
-evo_1 = evolution.Evolution([16, 10, 4])
+NN_size = [16, 10, 4]
+Num_Gen = 20
+Num_Pop = 1000
+M_Rate = 0.1
 
-Results = evo_1.evolve(2, 100, 0.05, save_file = True)
+evo_1 = evolution.Evolution(NN_size)
 
-with open("./Results/R_2_100_005.in", 'w') as f:
+Results = evo_1.evolve(Num_Gen, Num_Pop, M_Rate, save_file = True)
+
+with open("./Results/training_1.in", 'w') as f:
+	f.write(str(NN_size) + " " + str(Num_Gen) + " " + str(Num_Pop) + " " + str(M_Rate))
 	for a in range(len(Results)):
 		f.write("\n" + str(Results[a]))
