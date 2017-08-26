a1e2a5e0858c9a3532de53b123418bc465383ca7
ericlincc <lincheukyin_eric@yahoo.com.hk>
2017-08-27 01:59:44 +0800

    cross-breeding top 2

--- Evolutions.py
+++ Evolutions.py
@@ -13,39 +13,27 @@ class Evolution(object):
 			self.network_evolve(num_populations, mutation_rate)
 			
 	def network_evolve(self, num_populations, mutation_rate):
-		num_avg = int(num_populations / 10)
+		num_avg = 10
 		network_1 = self.network
-		#network_2 = self.network
-		avg_move_score_1 = [0.0, 0.0]
-		#avg_move_score_2 = [0.0, 0.0] 
-		for x in range(num_avg):
-			move_score_1 = self.play_game_network(network_1)
-			avg_move_score_1[0] = avg_move_score_1[0] + \
-					move_score_1[0] / num_avg
-			avg_move_score_1[1] = avg_move_score_1[1] + \
-					move_score_1[1] / num_avg
-			"""move_score_2 = play_game_network(network_2)
-			avg_move_score_2[0] = avg_move_score_2[0] + \
-					move_score_2[0] / num_avg
-			avg_move_score_2[1] = avg_move_score_2[1] + \
-					move_score_2[1] / num_avg
-			"""
+		network_2 = self.network
+		avg_move_score_1 = self.avg_move_score(network_1, num_avg)
+		avg_move_score_2 = self.avg_move_score(network_2, num_avg)
 		for i in range(num_populations):
-			new_network = network_1 #.mutate(mutation_rate)
-			new_avg_move_score = [0.0, 0.0]
-			for x in range(num_avg):
-				new_move_score = self.play_game_network(new_network)
-				new_avg_move_score[0] = new_avg_move_score[0] + \
-					new_move_score[0] / num_avg
-				new_avg_move_score[1] = new_avg_move_score[1] + \
-					new_move_score[1] / num_avg
-			#if (new_avg_move_score[1] > avg_move_score_2[1]):
-			if (new_avg_move_score[1] > avg_move_score_1[1]):
-				network_1 = new_network
-			#else:
-			#		network_2 = new_network
+			new_network = self.network
+			new_network.mutate(mutation_rate)
+			new_avg_move_score = self.avg_move_score(new_network, num_avg)
+			if (new_avg_move_score[1] > avg_move_score_2[1]):
+				if (new_avg_move_score[1] > avg_move_score_1[1]):
+					network_1 = new_network
+				else:
+					network_2 = new_network
 		print(new_avg_move_score)
-		self.network = network_1
+		i = 0
+		for b_1, w_1, b_2, w_2 in zip(network_1.biases, network_1.weights,  
+				network_2.biases, network_2.weights):
+			self.network.biases[i] = (b_1 + b_2) * 0.5
+			self.network.weights[i] = (w_1 + w_2) * 0.5
+			i = i + 1
 
 	def play_game_random(self):
 		play_board = game.new_game()
@@ -73,3 +61,12 @@ class Evolution(object):
 			##print(play_board[1], '\n', play_board[0])
 		return play_board[1]
 		
+	def avg_move_score(self, game_network, num_of_plays):
+		avg_move_scores = [0.0, 0.0]
+		for x in range(num_of_plays):
+			move_score = self.play_game_network(game_network)
+			avg_move_scores[0] = avg_move_scores[0] + \
+					move_score[0] / num_of_plays
+			avg_move_scores[1] = avg_move_scores[1] + \
+					move_score[1] / num_of_plays
+		return avg_move_scores
